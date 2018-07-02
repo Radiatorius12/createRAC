2d9974fe0c317f00c7d0eace5d8011dcbc9de546
Tony Adolph <33353284+tonyadolph@users.noreply.github.com>
2018-07-02 14:59:43 +0400

    Add files via upload

--- createDiskMapping.py
+++ createDiskMapping.py
@@ -7,23 +7,19 @@ Example:
 
 cd /home/automate/ovm/createRAC
 
-cat myBillsUAT_OVM.json
+cat eximBillsUAT_OVM.json
 {
-    "ovmHost":           "x.y.z.60"
+    "ovmHost":           "10.119.9.60"
     ,"port":              7002
     ,"baseUri":           "/ovm/core/wsapi/rest"
     ,"ovm_user":          "admin"
-    ,"ovm_pw":            "<password>"
-    ,"templateName":      "OVM_OL7U5_X86_64_12102DBRAC_PVHVM-1of2.tar.gz"
-    ,"templateNameId":    null
-    ,"repository":	      "UAT_POC_Repo"
-    ,"repositoryId":      null
-    ,"repositoryIdObj":   null
-    ,"serverPool":	      "UAT_POC"
-    ,"serverPoolId":      null
 }
 
-./createDiskMapping.py -ovmConfig myBillsUAT_OVM.json -wwid 600601600ec03600d5cce9f6b573e811 -vmId 0004fb0000060000cfc71a951de69201
+./createDiskMapping.py -c UAT_OVM.json -wwid 600601600E3600CC2153EFA67AE811 -vmId 0004fb0000060000cfc71a951de69201
+./createDiskMapping.py -c UAT_OVM.json -wwid 600601600ec03600cb545b949352e811 -vmId 0004fb00000600004ea7d13045e4d77e
+
+./createDiskMapping.py -c UAT_OVM.json -wwid 600601600ec036005bbdbdb89352e811 -vmName racphys0
+
 
 
 Status:
@@ -36,14 +32,24 @@ import json
 import argparse
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
+import getpass
+try:
+	import passwdPkg
+except Exception as e:
+	print "Warning: could not load passwdPkg : {}".format(e)
 import createRACnodes
 
 parser = argparse.ArgumentParser(description="Create Mapping between VM and Physical Disk (wwid)")
-parser.add_argument("-ovmConfig"       ,help="OVM json config file", required=True)
-parser.add_argument("-vmId"            ,help="OVM ID of Vm (get from OVM)", required=True)
-parser.add_argument("-wwid"            ,help="WWID of Physical Disk (get from SAN team)", required=True)
+parser.add_argument("-ovmConfig","-c"  ,help="OVM json config file", required=True)
 parser.add_argument("-debug"           ,help="set debugging", action="store_true")
+
+disk_group =  parser.add_mutually_exclusive_group(required=True)
+disk_group.add_argument("-wwid", "-w"          ,help="WWID of Physical Disk (get from SAN team)")
+disk_group.add_argument("-listofWWIDs", "-l"   ,help="file containing list WWIDs of Physical Disks (get from SAN team)")
+
+vm_group =  parser.add_mutually_exclusive_group(required=True)
+vm_group.add_argument("-vmId","-id"             ,help="OVM ID of Vm (get from OVM)")
+vm_group.add_argument("-vmName","-name","-n"    ,help="OVM name of Vm (get from OVM)")
 
 args = parser.parse_args()
 
@@ -57,20 +63,27 @@ debug = createRACnodes.Debug(args.debug)
 try:
 	with open(args.ovmConfig, "r") as f:
 		myConfig = json.load(f)
-
-except Exception as error:
-	print("Failed to load RAC Config from file {} error-> {}".format(conf.getName(), error))
-	exit()
+except ValueError as e:
+	print "The ovmConfig file \"{}\" is not valid json".format(args.ovmConfig)
+	exit(1)
 
 '''
 	Connect to OVM
 '''
+if myConfig.get("ovm_pw") is None:
+	ovm_vault_name = myConfig.get("ovm_vault_name")
+	if ovm_vault_name is not None:
+		try:
+			myConfig["ovm_pw"]=passwdPkg.getOVMPasswd(ovm_vault_name,'admin')
+		except Exception as e:
+			print e
+		if myConfig.get("ovm_pw") is None:
+			myConfig["ovm_pw"] = getpass.getpass("OVM admin password: ")
 
 s=requests.Session()
 s.auth=( myConfig["ovm_user"], myConfig["ovm_pw"] )
 s.verify=False #disables SSL certificate verification
 s.headers.update({'Accept': 'application/json', 'Content-Type': 'application/json'})
-
 baseUri="https://{host}:{port}{uri}".format(host=myConfig["ovmHost"],
 											 port=myConfig["port"],
 											 uri=myConfig['baseUri'])
@@ -80,20 +93,43 @@ baseUri="https://{host}:{port}{uri}".format(host=myConfig["ovmHost"],
 	Get StorageElement object for the Disk with WWID
 '''
 
-# Its array due to implementation in createRACnodes - but here we can only map one disk at the moment.
-diskToBeMapped = [ {
-							"name":          "ASM_DISK1"
+diskToBeMapped = []
+if args.listofWWIDs:
+	wwids = []
+	with open(args.listofWWIDs) as f:
+		wwids = f.read().splitlines()
+	diskNo = 0
+	for line in wwids:
+		if len(line) < 1:
+			continue
+		diskNo += 1
+		details=line.split(',')
+		wwid=details[0]
+		description=""
+		if len(details) > 1:
+			description=details[1]
+		diskToBeMapped.append({
+							"name":          "ASM_DISK{}".format({diskNo})
 							,"diskType":     "PHYSICAL_DISK"
 							,"size":         0
 							,"shareable":    True
 							,"sparse":       "No"
-							,"description":  ""
+							,"description":  "{}".format({description})
 							,"obj":          None
-							,"wwid":         ""
-					} ]
+							,"wwid":         wwid
+					})
+else:
+	diskToBeMapped = [ {
+								"name":          "ASM_DISK1"
+								,"diskType":     "PHYSICAL_DISK"
+								,"size":         0
+								,"shareable":    True
+								,"sparse":       "No"
+								,"description":  ""
+								,"obj":          None
+								,"wwid":         args.wwid
+						} ]
 
-#diskToBeMapped[0]["obj"] = createRACnodes.getDiskObjByWwid(s,baseUri,args.wwid)
-diskToBeMapped[0]["wwid"] = args.wwid
 
 debug.prt ( "Disk Object:"                                                   )
 debug.prt ( "==============================================================" )
@@ -103,13 +139,16 @@ debug.prt ( "==============================================================" )
 '''
 	Get vm object 
 '''
-racNodeObj=createRACnodes.get_obj_from_id(s,baseUri,"Vm",args.vmId)
-
+racNodeObj = ""
+if args.vmId is not None:
+	racNodeObj=createRACnodes.get_obj_from_id(s,baseUri,"Vm",args.vmId)
+else:
+	racNodeObj=createRACnodes.get_obj_from_name(s,baseUri,"Vm",args.vmName)
+	
 debug.prt ( "Vm Object:"                                                     )
 debug.prt ( "==============================================================" )
 debug.prt ( ( json.dumps(racNodeObj, indent=2) )                             )
 debug.prt ( "==============================================================" )
-
 
 createRACnodes.createVmDiskMapping(s,baseUri,racNodeObj,diskToBeMapped)
 
